4a1c77c61cf2fb8d3789c303718100df5b7f5c80
YatharthDesai <36489137+YatharthDesai@users.noreply.github.com>
2020-04-01 22:11:00 -0400

    Assignment 8 finish

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname + '/dist/wbdv-sp20-yatharth-client-angular'));
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+
-                           '/dist/wbdv-sp20-yatharth-client-angular/index.html'));});
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
@@ -21,4 +17,4 @@ app.get('/Get', (req, res) => res.send('Get called'))
 require('./controllers/quizzes.controller.server')(app)
 require('./controllers/questions.controller.server')(app)
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
