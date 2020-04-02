08bcce108f2d13493e7a55713fbdfc29b0551ad1
YatharthDesai <36489137+YatharthDesai@users.noreply.github.com>
2020-04-01 21:55:45 -0400

    Assignment 8 finish

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static(__dirname + '/dist/wbdv-sp20-yatharth-client-angular'));
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+
+                           '/dist/wbdv-sp20-yatharth-client-angular/index.html'));});
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
                "https://vast-mountain-24464.herokuapp.com/");
