c3f67a34821fbbdedc51536507e8191ecfc8cc34
YatharthDesai <36489137+YatharthDesai@users.noreply.github.com>
2020-04-11 21:35:03 -0400

    Assignment 9 update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://heroku_79pr6g05:j7gqjb791uiahoup0r3mk4t2g3@ds153667.
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-               "https://vast-mountain-24464.herokuapp.com/");
+               "https://vast-mountain-24464.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
                "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
